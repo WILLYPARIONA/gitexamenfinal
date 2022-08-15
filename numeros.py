ad71ef2d401883b02d56d6f71291c60bc8c59c44
willypariona <willypariona2611@gmail.com>
2022-08-15 14:35:44 -0500

    cargando archivos

---
+++ numeros.py
@@ -0,0 +1,29 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+
+type(5)
+<class 'int'>
+type(5.0)
+<class 'float'>
+type(8.8)
+<class 'float'>
+
+type(3.)
+<class 'float'>
+
+type(int(7.0))
+<class 'int'>
+
+type(float(3))
+<class 'float'>
+
+
+int(3.5)
+3
+int(3.999)
+3
+
+
+int(-8.7)
+-8
---
+++ puntosextra2.py
@@ -0,0 +1,5 @@
+nombre = "Willy"
+apellido = "Pariona"
+edad_años = 20
+numero_dni= 74754766
+print(" Me llamo {} mi apellido {} mi numero {}dni y tengo {} años ".format(nombre, apeliido, DNI, edad_años))
